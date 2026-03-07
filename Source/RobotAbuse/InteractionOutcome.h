678b2f42c20344c67b0f5d9c998ca778dc59afff
AAhlberg <ahlbergalexander@gmail.com>
2026-03-07 06:54:04 -0500

    Outcome header added and enum defined

---
+++ Source/RobotAbuse/InteractionOutcome.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "InteractionOutcome.generated.h"
+
+UENUM(BlueprintType)
+enum class EInteractionOutcome : uint8
+{
+	None      UMETA(DisplayName = "None"),
+	StartDrag UMETA(DisplayName = "Start Drag")
+};
